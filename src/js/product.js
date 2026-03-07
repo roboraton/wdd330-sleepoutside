614bf478a859e9959f03c66fe8c7b0fe4b107c05
roboraton <133822199+roboraton@users.noreply.github.com>
2026-03-07 12:47:08 -0600

    fix add to cart bug

--- src/js/product.js
+++ src/js/product.js
@@ -4,8 +4,16 @@ import ProductData from "./ProductData.mjs";
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  let carItems = JSON.parse(localStorage.getItem("so-cart"));
+
+  if (!Array.isArray(carItems)) {
+    carItems = [];
+  }
+
+  carItems.push(product);
+  setLocalStorage("so-cart", carItems);
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
