1f2a9a638e58259de672aed939727ae006bc3119
Percy2907 <pyarlequejara@byupathway.edu>
2026-03-20 19:51:34 -0500

    feat: implement dynamic header and footer using partials and AJAX

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 import ProductData from './ProductData.mjs';
 import ProductList from './ProductList.mjs';
-import { qs } from './utils.mjs';
+import { qs, loadHeaderFooter } from './utils.mjs';
+loadHeaderFooter();
 
 const dataSource = new ProductData('tents');
 const listElement = qs('.product-list');
--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -28,6 +28,31 @@ export function getParam(param) {
   return urlParams.get(param);
 }
 
+export function renderWithTemplate(template, parentElement, data, callback) {
+  parentElement.insertAdjacentHTML("afterbegin", template);
+  if (callback) {
+    callback(data);
+  }
+}
+
+export async function loadTemplate(path) {
+  const res = await fetch(path);
+  const template = await res.text();
+  return template;
+}
+
+export async function loadHeaderFooter() {
+
+  const headerTemplate = await loadTemplate("/partials/header.html");
+  const footerTemplate = await loadTemplate("/partials/footer.html");
+
+  const headerElement = document.querySelector("#main-header");
+  const footerElement = document.querySelector("#main-footer");
+
+  renderWithTemplate(headerTemplate, headerElement);
+  renderWithTemplate(footerTemplate, footerElement);
+}
+
 export function renderListWithTemplate(templateFn, parentElement, list, position = "afterbegin", clear = false) {
   if (clear) {
     parentElement.innerHTML = '';
